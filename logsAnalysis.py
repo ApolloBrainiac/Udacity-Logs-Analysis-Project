9b95accec82c09f0e77d53254dcc27baa9fbc269
Andrew M. Perez <AxMPerez@gmail.com>
2018-06-11 16:04:12 -0400

    Write SQL query for finding popular articles

--- logsAnalysis.py
+++ logsAnalysis.py
@@ -13,3 +13,17 @@ def db_connect():
         return cursor
 
 
+# What are the three most popular articles of all time?
+query1 = """
+         SELECT articles.title, COUNT(*) AS views
+         FROM articles
+         INNER JOIN log ON log.path ILIKE CONCAT('%', articles.slug, '%')
+         WHERE log.status ILIKE '%200%'
+         GROUP BY articles.title
+         ORDER BY views DESC
+         LIMIT 3;
+         """
+
+
+def pop_art(cursor):
+    cursor.execute(query1)
